e176bcb031a660cc1abf48cecacdfd76f046ccad
xhanxsarawak <info@mysati.io>
2024-05-14 08:00:29 +0500

    hover

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="logo-container">
-          <img src="maya0.svg" alt="MayaGPT Logo" className="logo gpt-logo" />
+          <a href="https://www.mayagpt.ai/login" target="_blank" rel="noopener noreferrer">
+            <img src="maya0.svg" alt="MayaGPT Logo" className="logo gpt-logo" />
+          </a>
           <a href="https://www.mayagpt.ai/login" target="_blank" rel="noopener noreferrer">
             <img src="maya1.svg" alt="Chat" className="icon gpt-chat" />
           </a>
         </div>
 
         <div className="logo-container">
-          <img src="maya2.svg" alt="MayaStudio Logo" className="logo studio-logo" />
+          <a href="https://www.mayastudio.ai/" target="_blank" rel="noopener noreferrer">
+            <img src="maya2.svg" alt="MayaStudio Logo" className="logo studio-logo" />
+          </a>
           <a href="https://www.mayastudio.ai/" target="_blank" rel="noopener noreferrer">
             <img src="maya3.svg" alt="Create" className="icon studio-create" />
           </a>
